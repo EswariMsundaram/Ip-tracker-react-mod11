3adfa4c6186b997cc43db37f071d95de6b2c963f
EswariMsundaram <eswarimsundaram@gmail.com>
2025-11-25 13:01:03 -0700

    Updated key and domain regex added

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,29 @@
-const API_KEY="at_EYDcjQM8mkAoBTho7RnMXWg9wZ585"
+const API_KEY="at_xSTYSfFahRCI3lCJZyAv6BKS3uTYY"
 
-export async function fetchIpData(domain=""){
-    const url=domain ? `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${domain}`
-                     : `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}`
 
-    const response=await fetch(url)
-    if(!response.ok){
-        throw new Error("Failed to fetch IP data")
-    }
-    return await response.json()
+
+export async function fetchIpData(domain = "") {
+  
+  let url;
+
+  // If no input - user's IP
+  if (!domain) {
+    url = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}`;
+  } 
+
+  // If input is an IP
+  else if (/^\d{1,3}(\.\d{1,3}){3}$/.test(domain)) {
+    url = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${domain}`;
+  } 
+  // Otherwise treat it as a domain
+  else {
+    url = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&domain=${domain}`;
+  }
+
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error("Failed to fetch IP/Domain data");
+  }
+
+  return await response.json();
 }
